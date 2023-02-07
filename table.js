be0b25787f3db49972e7ae80ac89d35c25f8008d
rakib <rakibcse192>
2023-02-07 23:21:27 +0600

    first commit

---
+++ table.js
@@ -0,0 +1,346 @@
+ //-----Bangla 1st
+
+ function calculation1() {
+     var _thm1 = document.getElementById('thm1').value;
+     var _prac1 = document.getElementById('prac1').value;
+     var _tm = parseInt(_thm1) + parseInt(_prac1);
+     document.getElementById('tm1').value = _tm;
+
+
+     if (_tm >= 80 && _tm <= 100) {
+         var _g1 = document.getElementById('g1').value = "A+";
+         var _p1 = document.getElementById('p1').value = "5.00";
+     } else if (_tm >= 70 && _tm <= 79) {
+         var _g1 = document.getElementById('g1').value = "A";
+         var _p1 = document.getElementById('p1').value = "4.00";
+     } else if (_tm >= 60 && _tm <= 69) {
+         var _g1 = document.getElementById('g1').value = "A-";
+         var _p1 = document.getElementById('p1').value = "3.50";
+     } else if (_tm >= 50 && _tm <= 59) {
+         var _g1 = document.getElementById('g1').value = "B";
+         var _p1 = document.getElementById('p1').value = "3.00";
+     } else if (_tm >= 40 && _tm <= 49) {
+         var _g1 = document.getElementById('g1').value = "C";
+         var _p1 = document.getElementById('p1').value = "2.00";
+     } else if (_tm >= 33 && _tm <= 39) {
+         var _g1 = document.getElementById('g1').value = "D";
+         var _p1 = document.getElementById('p1').value = "1.00";
+     } else if (_tm >= 0 && _tm <= 32) {
+         var _g1 = document.getElementById('g1').value = "F";
+         var _p1 = document.getElementById('p1').value = "0.00";
+     } else {
+         alert("Enter something wrong!");
+     }
+ }
+
+
+ //-----Bangla 2nd
+
+ function calculation2() {
+     var _thm2 = document.getElementById('thm2').value;
+     var _prac2 = document.getElementById('prac2').value;
+     var _tm = parseInt(_thm2) + parseInt(_prac2);
+     document.getElementById('tm2').value = _tm;
+
+
+     if (_tm >= 80 && _tm <= 100) {
+         var _g2 = document.getElementById('g2').value = "A+";
+         var _p2 = document.getElementById('p2').value = "5.00";
+     } else if (_tm >= 70 && _tm <= 79) {
+         var _g2 = document.getElementById('g2').value = "A";
+         var _p2 = document.getElementById('p2').value = "4.00";
+     } else if (_tm >= 60 && _tm <= 69) {
+         var _g2 = document.getElementById('g2').value = "A-";
+         var _p2 = document.getElementById('p2').value = "3.50";
+     } else if (_tm >= 50 && _tm <= 59) {
+         var _g2 = document.getElementById('g2').value = "B";
+         var _p2 = document.getElementById('p2').value = "3.00";
+     } else if (_tm >= 40 && _tm <= 49) {
+         var _g2 = document.getElementById('g2').value = "C";
+         var _p2 = document.getElementById('p2').value = "2.00";
+     } else if (_tm >= 33 && _tm <= 39) {
+         var _g2 = document.getElementById('g2').value = "D";
+         var _p2 = document.getElementById('p2').value = "1.00";
+     } else if (_tm >= 0 && _tm <= 32) {
+         var _g2 = document.getElementById('g2').value = "F";
+         var _p2 = document.getElementById('p2').value = "0.00";
+     } else {
+         alert("Enter something wrong!");
+     }
+ }
+
+ //-----English 1st
+
+ function calculation3() {
+     var _thm3 = document.getElementById('thm3').value;
+     var _prac3 = document.getElementById('prac3').value;
+     var _tm = parseInt(_thm3) + parseInt(_prac3);
+     document.getElementById('tm3').value = _tm;
+
+
+     if (_tm >= 80 && _tm <= 100) {
+         var _g3 = document.getElementById('g3').value = "A+";
+         var _p3 = document.getElementById('p3').value = "5.00";
+     } else if (_tm >= 70 && _tm <= 79) {
+         var _g3 = document.getElementById('g3').value = "A";
+         var _p3 = document.getElementById('p3').value = "4.00";
+     } else if (_tm >= 60 && _tm <= 69) {
+         var _g3 = document.getElementById('g3').value = "A-";
+         var _p3 = document.getElementById('p3').value = "3.50";
+     } else if (_tm >= 50 && _tm <= 59) {
+         var _g3 = document.getElementById('g3').value = "B";
+         var _p3 = document.getElementById('p3').value = "3.00";
+     } else if (_tm >= 40 && _tm <= 49) {
+         var _g3 = document.getElementById('g3').value = "C";
+         var _p3 = document.getElementById('p3').value = "2.00";
+     } else if (_tm >= 33 && _tm <= 39) {
+         var _g3 = document.getElementById('g3').value = "D";
+         var _p3 = document.getElementById('p3').value = "1.00";
+     } else if (_tm >= 0 && _tm <= 32) {
+         var _g3 = document.getElementById('g3').value = "F";
+         var _p3 = document.getElementById('p3').value = "0.00";
+     } else {
+         alert("Enter something wrong!");
+     }
+ }
+
+ //-----English 2nd
+
+ function calculation4() {
+     var _thm4 = document.getElementById('thm4').value;
+     var _prac4 = document.getElementById('prac4').value;
+     var _tm = parseInt(_thm4) + parseInt(_prac4);
+     document.getElementById('tm4').value = _tm;
+
+
+     if (_tm >= 80 && _tm <= 100) {
+         var _g4 = document.getElementById('g4').value = "A+";
+         var _p4 = document.getElementById('p4').value = "5.00";
+     } else if (_tm >= 70 && _tm <= 79) {
+         var _g4 = document.getElementById('g4').value = "A";
+         var _p4 = document.getElementById('p4').value = "4.00";
+     } else if (_tm >= 60 && _tm <= 69) {
+         var _g4 = document.getElementById('g4').value = "A-";
+         var _p4 = document.getElementById('p4').value = "3.50";
+     } else if (_tm >= 50 && _tm <= 59) {
+         var _g4 = document.getElementById('g4').value = "B";
+         var _p4 = document.getElementById('p4').value = "3.00";
+     } else if (_tm >= 40 && _tm <= 49) {
+         var _g4 = document.getElementById('g4').value = "C";
+         var _p4 = document.getElementById('p4').value = "2.00";
+     } else if (_tm >= 33 && _tm <= 39) {
+         var _g4 = document.getElementById('g4').value = "D";
+         var _p4 = document.getElementById('p4').value = "1.00";
+     } else if (_tm >= 0 && _tm <= 32) {
+         var _g4 = document.getElementById('g4').value = "F";
+         var _p4 = document.getElementById('p4').value = "0.00";
+     } else {
+         alert("Enter something wrong!");
+     }
+ }
+
+ //-----Higher math 1st
+
+ function calculation5() {
+     var _thm5 = document.getElementById('thm5').value;
+     var _prac5 = document.getElementById('prac5').value;
+     var _tm = parseInt(_thm5) + parseInt(_prac5);
+     document.getElementById('tm5').value = _tm;
+
+
+     if (_tm >= 80 && _tm <= 100) {
+         var _g5 = document.getElementById('g5').value = "A+";
+         var _p5 = document.getElementById('p5').value = "5.00";
+     } else if (_tm >= 70 && _tm <= 79) {
+         var _g5 = document.getElementById('g5').value = "A";
+         var _p5 = document.getElementById('p5').value = "4.00";
+     } else if (_tm >= 60 && _tm <= 69) {
+         var _g5 = document.getElementById('g5').value = "A-";
+         var _p5 = document.getElementById('p5').value = "3.50";
+     } else if (_tm >= 50 && _tm <= 59) {
+         var _g5 = document.getElementById('g5').value = "B";
+         var _p5 = document.getElementById('p5').value = "3.00";
+     } else if (_tm >= 40 && _tm <= 49) {
+         var _g5 = document.getElementById('g5').value = "C";
+         var _p5 = document.getElementById('p5').value = "2.00";
+     } else if (_tm >= 33 && _tm <= 39) {
+         var _g5 = document.getElementById('g5').value = "D";
+         var _p5 = document.getElementById('p5').value = "1.00";
+     } else if (_tm >= 0 && _tm <= 32) {
+         var _g5 = document.getElementById('g5').value = "F";
+         var _p5 = document.getElementById('p5').value = "0.00";
+     } else {
+         alert("Enter something wrong!");
+     }
+ }
+
+
+ //-----Higher Math 2nd
+
+ function calculation6() {
+     var _thm6 = document.getElementById('thm6').value;
+     var _prac6 = document.getElementById('prac6').value;
+     var _tm = parseInt(_thm6) + parseInt(_prac6);
+     document.getElementById('tm6').value = _tm;
+
+
+     if (_tm >= 80 && _tm <= 100) {
+         var _g6 = document.getElementById('g6').value = "A+";
+         var _p6 = document.getElementById('p6').value = "5.00";
+     } else if (_tm >= 70 && _tm <= 79) {
+         var _g6 = document.getElementById('g6').value = "A";
+         var _p6 = document.getElementById('p6').value = "4.00";
+     } else if (_tm >= 60 && _tm <= 69) {
+         var _g6 = document.getElementById('g6').value = "A-";
+         var _p6 = document.getElementById('p6').value = "3.50";
+     } else if (_tm >= 50 && _tm <= 59) {
+         var _g6 = document.getElementById('g6').value = "B";
+         var _p6 = document.getElementById('p6').value = "3.00";
+     } else if (_tm >= 40 && _tm <= 49) {
+         var _g6 = document.getElementById('g6').value = "C";
+         var _p6 = document.getElementById('p6').value = "2.00";
+     } else if (_tm >= 33 && _tm <= 39) {
+         var _g6 = document.getElementById('g6').value = "D";
+         var _p6 = document.getElementById('p6').value = "1.00";
+     } else if (_tm >= 0 && _tm <= 32) {
+         var _g6 = document.getElementById('g6').value = "F";
+         var _p6 = document.getElementById('p6').value = "0.00";
+     } else {
+         alert("Enter something wrong!");
+     }
+ }
+ //-----Physics 1st
+
+ function calculation7() {
+     var _thm7 = document.getElementById('thm7').value;
+     var _prac7 = document.getElementById('prac7').value;
+     var _tm = parseInt(_thm7) + parseInt(_prac7);
+     document.getElementById('tm7').value = _tm;
+
+
+     if (_tm >= 80 && _tm <= 100) {
+         var _g7 = document.getElementById('g7').value = "A+";
+         var _p7 = document.getElementById('p7').value = "5.00";
+     } else if (_tm >= 70 && _tm <= 79) {
+         var _g7 = document.getElementById('g7').value = "A";
+         var _p7 = document.getElementById('p7').value = "4.00";
+     } else if (_tm >= 60 && _tm <= 69) {
+         var _g7 = document.getElementById('g7').value = "A-";
+         var _p7 = document.getElementById('p7').value = "3.50";
+     } else if (_tm >= 50 && _tm <= 59) {
+         var _g7 = document.getElementById('g7').value = "B";
+         var _p7 = document.getElementById('p7').value = "3.00";
+     } else if (_tm >= 40 && _tm <= 49) {
+         var _g7 = document.getElementById('g7').value = "C";
+         var _p7 = document.getElementById('p7').value = "2.00";
+     } else if (_tm >= 33 && _tm <= 39) {
+         var _g7 = document.getElementById('g7').value = "D";
+         var _p7 = document.getElementById('p7').value = "1.00";
+     } else if (_tm >= 0 && _tm <= 32) {
+         var _g7 = document.getElementById('g7').value = "F";
+         var _p7 = document.getElementById('p7').value = "0.00";
+     } else {
+         alert("Enter something wrong!");
+     }
+ }
+
+ //-----Physics 2nd
+
+ function calculation8() {
+     var _thm8 = document.getElementById('thm8').value;
+     var _prac8 = document.getElementById('prac8').value;
+     var _tm = parseInt(_thm8) + parseInt(_prac8);
+     document.getElementById('tm8').value = _tm;
+
+
+     if (_tm >= 80 && _tm <= 100) {
+         var _g8 = document.getElementById('g8').value = "A+";
+         var _p8 = document.getElementById('p8').value = "5.00";
+     } else if (_tm >= 70 && _tm <= 79) {
+         var _g8 = document.getElementById('g8').value = "A";
+         var _p8 = document.getElementById('p8').value = "4.00";
+     } else if (_tm >= 60 && _tm <= 69) {
+         var _g8 = document.getElementById('g8').value = "A-";
+         var _p8 = document.getElementById('p8').value = "3.50";
+     } else if (_tm >= 50 && _tm <= 59) {
+         var _g8 = document.getElementById('g8').value = "B";
+         var _p8 = document.getElementById('p8').value = "3.00";
+     } else if (_tm >= 40 && _tm <= 49) {
+         var _g8 = document.getElementById('g8').value = "C";
+         var _p8 = document.getElementById('p8').value = "2.00";
+     } else if (_tm >= 33 && _tm <= 39) {
+         var _g8 = document.getElementById('g8').value = "D";
+         var _p8 = document.getElementById('p8').value = "1.00";
+     } else if (_tm >= 0 && _tm <= 32) {
+         var _g8 = document.getElementById('g8').value = "F";
+         var _p8 = document.getElementById('p8').value = "0.00";
+     } else {
+         alert("Enter something wrong!");
+     }
+ }
+ //---chemistry 1st
+ function calculation9() {
+     var _thm9 = document.getElementById('thm9').value;
+     var _prac9 = document.getElementById('prac9').value;
+     var _tm = parseInt(_thm9) + parseInt(_prac9);
+     document.getElementById('tm9').value = _tm;
+
+
+     if (_tm >= 80 && _tm <= 100) {
+         var _g9 = document.getElementById('g9').value = "A+";
+         var _p9 = document.getElementById('p9').value = "5.00";
+     } else if (_tm >= 70 && _tm <= 79) {
+         var _g9 = document.getElementById('g9').value = "A";
+         var _p9 = document.getElementById('p9').value = "4.00";
+     } else if (_tm >= 60 && _tm <= 69) {
+         var _g9 = document.getElementById('g9').value = "A-";
+         var _p9 = document.getElementById('p9').value = "3.50";
+     } else if (_tm >= 50 && _tm <= 59) {
+         var _g2 = document.getElementById('g9').value = "B";
+         var _p2 = document.getElementById('p9').value = "3.00";
+     } else if (_tm >= 40 && _tm <= 49) {
+         var _g9 = document.getElementById('g9').value = "C";
+         var _p9 = document.getElementById('p9').value = "2.00";
+     } else if (_tm >= 33 && _tm <= 39) {
+         var _g9 = document.getElementById('g9').value = "D";
+         var _p9 = document.getElementById('p9').value = "1.00";
+     } else if (_tm >= 0 && _tm <= 32) {
+         var _g9 = document.getElementById('g9').value = "F";
+         var _p9 = document.getElementById('p9').value = "0.00";
+     } else {
+         alert("Enter something wrong!");
+     }
+ }
+ //---chemistry 2nd
+ function calculation10() {
+     var _thm10 = document.getElementById('thm10').value;
+     var _prac10 = document.getElementById('prac10').value;
+     var _tm = parseInt(_thm10) + parseInt(_prac10);
+     document.getElementById('tm10').value = _tm;
+
+
+     if (_tm >= 80 && _tm <= 100) {
+         var _g10 = document.getElementById('g10').value = "A+";
+         var _p10 = document.getElementById('p10').value = "5.00";
+     } else if (_tm >= 70 && _tm <= 79) {
+         var _g10 = document.getElementById('g10').value = "A";
+         var _p10 = document.getElementById('p10').value = "4.00";
+     } else if (_tm >= 60 && _tm <= 69) {
+         var _g10 = document.getElementById('g10').value = "A-";
+         var _p10 = document.getElementById('p10').value = "3.50";
+     } else if (_tm >= 50 && _tm <= 59) {
+         var _g10 = document.getElementById('g10').value = "B";
+         var _p10 = document.getElementById('p10').value = "3.00";
+     } else if (_tm >= 40 && _tm <= 49) {
+         var _g10 = document.getElementById('g10').value = "C";
+         var _p10 = document.getElementById('p10').value = "2.00";
+     } else if (_tm >= 33 && _tm <= 39) {
+         var _g10 = document.getElementById('g10').value = "D";
+         var _p10 = document.getElementById('p10').value = "1.00";
+     } else if (_tm >= 0 && _tm <= 32) {
+         var _g10 = document.getElementById('g10').value = "F";
+         var _p10 = document.getElementById('p10').value = "0.00";
+     } else {
+         alert("Enter something wrong!");
+     }
+ }
